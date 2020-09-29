a9d0334cc1eb877a370e6643cc43ecf01c420238
Gabriel Neves <gabriel.bneves@gmail.com>
2020-09-29 09:29:57 +0100

    remove & filter

---
+++ app.js
@@ -0,0 +1,51 @@
+// define UI variables
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// load all event listeners
+loadEventListeners();
+
+//
+function loadEventListeners(){
+  //add task event
+  form.addEventListener('submit', addTask);
+  clearBtn.addEventListener('click', deleteTask);
+
+}
+
+//add task
+function addTask(e){
+
+  if(taskInput.value === ''){
+    alert('Add a task');
+  }
+
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+
+  taskInput.value = ''
+
+  e.preventDefault();
+
+}
+
+// delete task
+
+function deleteTask(e) {
+  
+}
+
+console.log(taskList.children);
