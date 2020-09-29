b9f37289b8769afa9c7bfb8bce5ce7d6c2a269ff
Gabriel Neves <gabriel.bneves@gmail.com>
2020-09-29 10:10:58 +0100

    persist to local

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('clear-tasks');
+const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
@@ -13,8 +13,9 @@ loadEventListeners();
 function loadEventListeners(){
   //add task event
   form.addEventListener('submit', addTask);
-  clearBtn.addEventListener('click', deleteTask);
-
+  taskList.addEventListener('click', deleteTask);  
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keydown', filterTasks)
 }
 
 //add task
@@ -45,7 +46,39 @@ function addTask(e){
 // delete task
 
 function deleteTask(e) {
-  
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
 
-console.log(taskList.children);
+function clearTasks(){
+  //easier
+  // taskList.innerHTML = '';
+  
+  //faster
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  // https://jsperf.com/innerhtml-vs-removechild
+
+}
+
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach((task) => {
+
+    const item = task.firstChild.textContent;
+
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+
+  })
+
+}
